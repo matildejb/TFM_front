4ad4d6f574838f958854a42ce125df13975ed986
Maria Moreno Ibáñez <mariamorenoibanez@gmail.com>
2024-05-27 15:08:15 +0200

    Componente del menú de botones
    Creación y comienzo de estilo del menú de botones de la página principal, esperando a la creación de rutas para enlazar los botones y a establecer qué botones necesitaremos

---
+++ src/app/components/menu/menu-buttons.components.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
+
+@Component({
+    selector: 'app-menu-buttons',
+    standalone: true,
+    imports: [RouterLink],
+    templateUrl: './menu-buttons.component.html',
+    styleUrl: './menu-buttons.component.css'
+})
+export class MenuButtonsComponent {
+    constructor() {}
+}
