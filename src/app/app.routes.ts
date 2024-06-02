6171a629fe95e5113c056f7fdb89be4b184dea6c
matildejb <matildejimenezbedmar@gmail.com>
2024-06-02 23:19:43 +0200

    Landing
    Typed.js (biblioteca js/  animaciones de texto)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './pages/users/welcome/welcome.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 export const routes: Routes = [
   {
@@ -7,5 +8,7 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./pages/users/users.routes').then((m) => m.USERS_ROUTES),
   },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: 'landing', component: LandingComponent },
   { path: 'welcome', component: WelcomeComponent },
 ];
---
+++ src/app/pages/landing/landing.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IntroComponent } from './intro/intro.component';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [IntroComponent],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.css'
+})
+export class LandingComponent {
+
+}
