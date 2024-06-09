9bc968f274c0c793e4cf1eabed24290317fbe114
Alberto Suárez <alberto.suarez.ing@gmail.com>
2024-06-09 20:16:46 +0200

    Funcionalidades del menu de ajustes
    He realizado funcionalidades en los botones del menu hamburger de ajustes de grupos, añadiendo un modal con 3 opciones de edición de grupos

--- src/app/pages/summary/summary.component.ts
+++ src/app/pages/summary/summary.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './summary.component.html',
-  styleUrl: './summary.component.css'
+  styleUrls: ['./summary.component.css']
 })
 export class SummaryComponent {
+  isGroupsModalOpen = false;
+  isFriendsModalOpen = false;
 
+  openGroupsModal() {
+    this.isGroupsModalOpen = true;
+    this.isFriendsModalOpen = false;
+  }
+
+  openFriendsModal() {
+    this.isFriendsModalOpen = true;
+    this.isGroupsModalOpen = false;
+  }
+	
+  closeGroupsModal() {
+    this.isGroupsModalOpen = false;
+  }
+
+  closeFriendsModal() {
+    this.isFriendsModalOpen = false;
+  }
 }
