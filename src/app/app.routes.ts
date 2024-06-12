7004f482338785bf6e73f65f7836a76640c7067d
Alberto Suárez <alberto.suarez.ing@gmail.com>
2024-06-12 13:40:24 +0200

    Configuración de rutas
    He configurado el fichero de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,29 +9,13 @@ import { GroupComponent } from './components/group/group.component';
 
 
 export const routes: Routes = [
-  {
-
-    //Configuración rutas,
-    //al tener dos rutas con el mismo  path: '' (vacio)
-    //Genera un conflicto y no se visualiza la landing al abrir app 
-    //he puesto users, COMENTARLO!!!
-    path: 'users',
-    loadChildren: () =>
-      import('./pages/users/users.routes').then((m) => m.USERS_ROUTES),
-  },
   { path: '', redirectTo: '/landing', pathMatch: 'full' },
   { path: 'landing', component: LandingComponent },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-    path: 'groupsList',
-    component: GroupsListComponent
-  },
-  {
-    path: 'group/:id',
-    component: GroupComponent
-  },
-  {
+  { path: 'groupsList', component: GroupsListComponent },
+  { path: 'group/:id', component: GroupComponent },
   { path: 'page-not-found', component: ErrorComponent },
   { path: '**', redirectTo: '/page-not-found' },
 ];
