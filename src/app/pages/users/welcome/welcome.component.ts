4c384e861f94d2d953ca633ad0b2622c5e97b0d1
matildejb <matildejimenezbedmar@gmail.com>
2024-06-14 19:02:51 +0200

    Footer solo en landing + welcome

--- src/app/pages/users/welcome/welcome.component.ts
+++ src/app/pages/users/welcome/welcome.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FooterComponent } from '../../../components/footer/footer.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css',
 })
