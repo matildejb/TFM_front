6a1ccae21d276caf4f9fd60f5af9db60408e3f81
Julia_mb <juliasav2023@gmail.com>
2024-06-19 12:58:28 +0200

    Crear componentes friends

---
+++ src/app/components/friends-card/friends-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friends-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './friends-card.component.html',
+  styleUrl: './friends-card.component.css'
+})
+export class FriendsCardComponent {
+
+}
