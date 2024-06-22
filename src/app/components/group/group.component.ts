39f042a5a537d88d42c6ad2d0f7b814e7f077ca0
Alberto Suárez <alberto.suarez.ing@gmail.com>
2024-06-22 14:42:20 +0200

    Ajustes de componente groupCard

--- src/app/components/group/group.component.ts
+++ src/app/components/group/group.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges, inject } from '@angular/core';
 import { GroupService } from '../../services/groups.service';
 import { IGroup } from '../../interfaces/igroup.interfaces';
 import { IPayment } from '../../interfaces/ipayments.interfaces';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { UsersService } from '../../services/users.service';
+import { IUser } from '../../interfaces/iuser.interfaces';
 
 
 @Component({
@@ -14,23 +16,44 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './group.component.html',
 })
 export class GroupComponent implements OnInit {
+  @Input() amount: number = 0;
+
   unGroup: IGroup | null = null;
+  arrGroups: IGroup[] = [];
   arrPayments: IPayment[] = [];
   groupService = inject(GroupService);
   group_id: number = 0;
+  payment: any;
+  TotalBalance: number = 0;
+  user_id: number = 1;
+  amountColor: string = 'green';
+  showNoPaymentsMessage: boolean = false;
 
   // paymentService = inject(PaymentService);
 
   constructor(
-    private route: ActivatedRoute // Inyecta ActivatedRoute
+    private route: ActivatedRoute,
+    private groupsService: GroupService,
+    private userService: UsersService
   ) { }
 
+  unUser: IUser | null = null;
+
+
   ngOnInit(): void {
+    this.getMyGroups();
     // Obtén el ID del grupo de la ruta
     this.route.params.subscribe(params => {
       this.group_id = +params['id'];
+      // this.getPayments(this.group_id);
       this.getPayments(this.group_id);
     });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['amount']) {
+      this.amountColor = this.amount < 0 ? 'Crimson' : 'Limegreen';
+    }
   }
 
   async getPayments(group_id: any): Promise<void> {
@@ -41,6 +64,21 @@ export class GroupComponent implements OnInit {
         this.unGroup = group;
         this.arrPayments = await this.groupService.getPayments(group_id);
         console.log('Mis grupos obtenidos:', this.arrPayments);
+        this.unGroup = group;
+        this.showNoPaymentsMessage = this.arrPayments.length === 0;
+      }
+    } catch (error) {
+      console.error('Error al obtener mis grupos:', error);
+    }
+  }
+
+  async getMyGroups(): Promise<void> {
+    try {
+      const user = await this.userService.getProfile();
+      if (user) {
+        this.unUser = user;
+        this.arrGroups = await this.groupService.getMyGroups(user.id);
+        console.log('Mis grupos obtenidos:', this.arrGroups);
       }
     } catch (error) {
       console.error('Error al obtener mis grupos:', error);
