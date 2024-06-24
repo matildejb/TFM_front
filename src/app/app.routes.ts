5402a9b37f490568564d7f8454183bc0fa5e5de8
Alberto Suárez <alberto.suarez.ing@gmail.com>
2024-06-24 23:54:17 +0200

    Correcciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,6 +18,7 @@ import { PaymentsComponent } from './pages/buttons/payments/payments.component';
 import { FormGroupComponent } from './pages/buttons/form-group/form-group.component';
 import { GroupMembersComponent } from './pages/group-members/group-members.component';
 import { FriendsComponent } from './pages/friends/friends.component';
+import { StatisticsComponent } from './pages/statistics_historial/statistics.component';
 
 export const routes: Routes = [
   { path: 'updateUser/:userId', component: RegisterComponent },
@@ -36,10 +37,8 @@ export const routes: Routes = [
   { path: 'group/:id', component: GroupComponent, canActivate: [loginGuard] },
   { path: 'group/:id/filter', component: FilterComponent, canActivate: [loginGuard] },
   { path: 'group/:id/groupMembers', component: GroupMembersComponent, canActivate: [loginGuard] },
-  {
-    path: 'friends', component: FriendsComponent, canActivate: [loginGuard]
-  },
-
+  { path: 'friends', component: FriendsComponent, canActivate: [loginGuard] },
+  { path: 'historial', component: StatisticsComponent, canActivate: [loginGuard] },
 
   { path: 'group/:id/formPayments', component: PaymentsComponent },
   { path: 'createGroup', component: FormGroupComponent },
